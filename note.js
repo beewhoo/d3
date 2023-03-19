6b7aba7a768e023fac90d0ac7d566bc0796701fb
Bibek Thapa <beewhoo@Bibeks-MacBook-Pro.local>
2023-03-19 12:42:49 -0400

    same region

---
+++ note.js
@@ -0,0 +1,33 @@
+// incoming data
+
+// [
+//     {team: 'germany', region: 'UEFA', win: 7 }, 
+//     {team: 'Argentina', region: 'CONEMBOL', win: 7}, 
+//     {team: 'Netherlands', region: 'UEFA', win: 7},    
+// ]
+
+
+
+
+
+// Object.keys
+
+// key => value pair 
+
+const object1 = {
+    a: 'somestring',
+    b: 42,
+    c: false,
+    d: 'apple'
+  };
+
+
+// object1.b  => 42
+// Object.keys(object1)  => [a , b, c, d ]
+
+  
+
+// 1 === 1 => true 
+// 1 === 2 =>  false 
+// 1 != 2  => true 
+// 1 != 1 => false 
--- soccerviz.js
+++ soccerviz.js
@@ -1,64 +1,79 @@
 function createSoccerViz() {
-  d3.csv("https://raw.githubusercontent.com/beewhoo/d3/master/worldcup.csv?token=GHSAT0AAAAAAB7OMKPDOX2YWKV6OUJDMQEKZATZTAA", function(data) {
-      overallTeamViz(data);
-  })
+    d3.csv("https://raw.githubusercontent.com/beewhoo/d3/master/worldcup.csv?token=GHSAT0AAAAAAB7OMKPDOX2YWKV6OUJDMQEKZATZTAA", function(data) {
+        overallTeamViz(data);
+    })
 
-  function overallTeamViz(incomingData) {
-      d3.select("svg")
-          .append("g")
-          .attr("id", "teamsG")
-          .attr("transform", "translate(50,300)")
-          .selectAll("g")
-          .data(incomingData)
-          .enter()
-          .append("g")
-          .attr("class", "overallG")
-          .attr("transform",
-              function(d, i) {
-                  return "translate(" + (i * 50) + ", 0)"
-              }
-          );
-      var teamG = d3.selectAll("g.overallG");
-      teamG
-          .append("circle")
-          .attr("r", 20)
-          .style("fill", "pink")
-          .style("stroke", "black")
-          .style("stroke-width", "1px");
-      teamG
-          .append("text")
-          .style("text-anchor", "middle")
-          .attr("y", 30)
-          .style("font-size", "10px")
-          .text(function(d) {
-              return d.team;
-          });
+    function overallTeamViz(incomingData) {
+        d3.select("svg")
+            .append("g")
+            .attr("id", "teamsG")
+            .attr("transform", "translate(50,300)")
+            .selectAll("g")
+            .data(incomingData)
+            .enter()
+            .append("g")
+            .attr("class", "overallG")
+            .attr("transform",
+                function(d, i) {
+                    return "translate(" + (i * 50) + ", 0)"
+                }
+            );
+        var teamG = d3.selectAll("g.overallG");
 
-      var dataKeys = d3.keys(incomingData[0]).filter(function(el) {
-          return el != "team" && el != "region";
-      });
+        teamG
+            .append("circle")
+            .attr("r", 20)
+            .style("fill", "pink")
+            .style("stroke", "black")
+            .style("stroke-width", "1px");
+        teamG
+            .append("text")
+            .style("text-anchor", "middle")
+            .attr("y", 30)
+            .style("font-size", "10px")
+            .text(function(d) {
+                return d.team;
+            });
 
-      d3.select("#controls").selectAll("button.teams")
-          .data(dataKeys).enter()
-          .append("button")
-          .on("click", buttonClick)
-          .html(function(d) {
-              return d;
-          });
+        var dataKeys = d3.keys(incomingData[0]).filter(function(el) {
+            return el != "team" && el != "region";
+        });
 
-      function buttonClick(datapoint) {
-          var maxValue = d3.max(incomingData, function(d) {
-              return parseFloat(d[datapoint]);
-          });
+        d3.select("#controls").selectAll("button.teams")
+            .data(dataKeys).enter()
+            .append("button")
+            .on("click", buttonClick)
+            .html(function(d) {
+                return d;
+            });
 
-          var radiusScale = d3.scale.linear()
-              .domain([0, maxValue])
-              .range([2, 20]);
+        function buttonClick(datapoint) {
+            var maxValue = d3.max(incomingData, function(d) {
+                return parseFloat(d[datapoint]);
+            });
 
-          d3.selectAll("g.overallG").select("circle").attr("r", function(d) {
-              return radiusScale(d[datapoint]);
-          });
-      };
+            var radiusScale = d3.scale.linear()
+                .domain([0, maxValue])
+                .range([2, 20]);
 
-  }
+            d3.selectAll("g.overallG").select("circle").attr("r", function(d) {
+                return radiusScale(d[datapoint]);
+            });
+        };
+
+        teamG.on("mouseover", highlightRegion);
+
+        function highlightRegion(mouseOverSelected) {
+            d3.selectAll("g.overallG").select("circle").style("fill", function(country) {
+                
+                if (country.region === mouseOverSelected.region) {
+                    return "red";
+                } else {
+                    return "gray";
+                }
+            });
+        
+        };
+
+    }
 }
